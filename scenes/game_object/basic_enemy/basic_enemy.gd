b689e2a45424e87c39576c1c971ecde5eb07d895
Ian <ian.wettingfeld@gmail.com>
2025-02-21 10:16:22 +0000

    Split out HitboxComponent and HurtboxComponent

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -7,7 +7,7 @@ const MAX_SPEED = 40
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Area2D.area_entered.connect(on_area_entered)
+	$HurtboxComponent.area_entered.connect(on_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
