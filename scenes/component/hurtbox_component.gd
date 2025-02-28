8349cfeb04e47af4881a861703bcc2cb6cd31b57
Ian <ian.wettingfeld@gmail.com>
2025-02-28 22:47:12 +0000

    Add SwordDamage and AxeDamage ability upgrades

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -4,6 +4,8 @@ class_name HurtboxComponent
 @export var health_component: HealthComponent
 
 var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
+var format_string_1dp = "%0.1f"
+var format_string_0dp = "%0.0f"
 
 
 func _ready():
@@ -24,4 +26,9 @@ func on_area_entered(other_area: Area2D):
 	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
 	
 	floating_text.global_position = global_position + (Vector2.UP * 16)
-	floating_text.start(str(hitbox_component.damage))
+	
+	var format_string = format_string_1dp
+	if round(hitbox_component.damage) == hitbox_component.damage:
+		format_string = format_string_0dp
+	
+	floating_text.start(format_string % hitbox_component.damage)
