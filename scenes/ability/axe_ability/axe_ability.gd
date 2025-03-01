b7af8c7b7a3e9130355b53758de7ecfc3cb7b512
Ian <ian.wettingfeld@gmail.com>
2025-03-01 20:43:48 +0000

    Add animations for upgrade cards

--- scenes/ability/axe_ability/axe_ability.gd
+++ scenes/ability/axe_ability/axe_ability.gd
@@ -18,7 +18,7 @@ func _ready():
 
 func tween_method(rotations: float):
 	var percent = rotations / 2
-	var current_radius = (rotations / 2) * MAX_RADIUS
+	var current_radius = percent * MAX_RADIUS
 	var current_direction = base_rotation.rotated(rotations * TAU)
 	
 	var player = get_tree().get_first_node_in_group("player") as Node2D
