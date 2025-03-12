9a5a4858269f6ad352a6cd05a5431ccfc693e25a
Ian <ian.wettingfeld@gmail.com>
2025-03-12 20:48:36 +0000

    Add anvil count ability upgrade

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -12,7 +12,7 @@ var base_wait_time
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	base_wait_time = $Timer.wait_time
-	$Timer.timeout.connect(on_timer_timeout)  # get_node("Timer")
+	$Timer.timeout.connect(on_timer_timeout)
 	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
